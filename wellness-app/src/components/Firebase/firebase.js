7cffaa8d35a0ea93b40d811491273ac17469e862
Marco Cardoso <mcardosog@icloud.com>
2019-11-07 19:14:38 -0500

    Working: SIGN UP WITH REACT AND FIREBASE

--- wellness-app/src/components/Firebase/firebase.js
+++ wellness-app/src/components/Firebase/firebase.js
@@ -1,5 +1,7 @@
 import app from 'firebase/app'
+import 'firebase/auth';
 
+//FireBase config
 const config = {
     apiKey: "AIzaSyCJdiZfsRDAhJraJU8XcDgikepmGj4hSc4",
     authDomain: "wellnessnetwork-35780.firebaseapp.com",
@@ -14,7 +16,19 @@ const config = {
 class Firebase {
     constructor() {
         app.initializeApp(config);
+        this.auth = app.auth();
     }
+
+    //Auth
+
+    doCreateUserWithEmailAndPassword = (email, password) =>
+        this.auth.createUserWithEmailAndPassword(email, password);
+    doSignInWithEmailAndPassword = (email, password) =>
+        this.auth.signInWithEmailAndPassword(email, password);
+    doSignOut = () => this.auth.signOut();
+    doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+    doPasswordUpdate = password =>
+        this.auth.currentUser.updatePassword(password);
 }
 
 export default Firebase;
--- wellness-app/src/components/SignUp/index.js
+++ wellness-app/src/components/SignUp/index.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import * as ROUTES from '../../constants/routes';
-const SignUp = () => (
+const SignUpPage = () => (
     <div>
-        SIGN UP
+        <h1>SignUp</h1>
+        <SignUpForm />
     </div>
 );
-export default SignUp;
+class SignUpForm extends Component {
+    constructor(props) {
+        super(props);
+    }
+    onSubmit = event => {
+    }
+    onChange = event => {
+    };
+    render() {
+        return (
+            <form onSubmit={this.onSubmit}>
+            </form>
+        );
+    }
+}
+const SignUpLink = () => (
+    <p>
+        Don't have an account? <Link to={ROUTES.SIGN_UP}>Sign Up</Link>
+    </p>
+);
+export default SignUpPage;
+export { SignUpForm, SignUpLink };
