21543d028178686ce33f50c874001a965b42e51e
Marco Cardoso <mcardosog@icloud.com>
2019-11-09 00:37:15 -0500

    Completed MANAGE USERS WITH FIREBASE'S REALTIME DATABASE IN REACT. Still don't know how to delete users from the admin page. Needed to add verification of user login before allow the user to open the page /admin

--- wellness-app/src/components/Firebase/firebase.js
+++ wellness-app/src/components/Firebase/firebase.js
@@ -1,5 +1,6 @@
 import app from 'firebase/app'
 import 'firebase/auth';
+import 'firebase/database';
 
 
 //FireBase config
@@ -17,6 +18,7 @@ class Firebase {
     constructor() {
         app.initializeApp(config);
         this.auth = app.auth();
+        this.db = app.database();
     }
 
     //Auth
@@ -29,6 +31,9 @@ class Firebase {
     doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
     doPasswordUpdate = password =>
         this.auth.currentUser.updatePassword(password);
+
+    user = uid => this.db.ref(`users/${uid}`);
+    users = () => this.db.ref('users');
 }
 
 export default Firebase;
