63bb5323fef0683514534130c4273418c7ec3f51
SprocketMan189 <coreykochman@gmail.com>
2019-11-10 15:54:46 -0500

    Added a file uploader to the Account Page

--- wellness-app/src/components/Account/index.js
+++ wellness-app/src/components/Account/index.js
@@ -1,11 +1,53 @@
-import React from 'react';
+import React, { Component } from "react";
 import { PasswordForgetForm } from '../PasswordForget';
 import PasswordChangeForm from '../PasswordChange';
-const AccountPage = () => (
-    <div>
-        <h1>Account Page</h1>
-        <PasswordForgetForm />
-        <PasswordChangeForm />
-    </div>
-);
-export default AccountPage;
+import firebase from "firebase";
+import FileUploader from "react-firebase-file-uploader";
+
+class Account extends Component {
+    state = {
+        avatar: "",
+        isUploading: false,
+        progress: 0,
+        avatarURL: ""
+    };
+
+    handleUploadStart = () => this.setState({ isUploading: true, progress: 0 });
+    handleProgress = progress => this.setState({ progress });
+    handleUploadError = error => {
+        this.setState({ isUploading: false });
+        console.error(error);
+    };
+    handleUploadSuccess = filename => {
+        this.setState({ avatar: filename, progress: 100, isUploading: false });
+        firebase
+            .storage()
+            .ref("images")
+            .child(filename)
+            .getDownloadURL()
+            .then(url => this.setState({ avatarURL: url }));
+    };
+
+    render() {
+        return (
+            <div>
+                <form>
+                    <PasswordForgetForm/>
+                    <PasswordChangeForm/>
+                    <FileUploader
+                        accept="image/*"
+                        name="avatar"
+                        randomizeFilename
+                        storageRef={firebase.storage().ref("images")}
+                        onUploadStart={this.handleUploadStart}
+                        onUploadError={this.handleUploadError}
+                        onUploadSuccess={this.handleUploadSuccess}
+                        onProgress={this.handleProgress}
+                    />
+                </form>
+            </div>
+        );
+    }
+}
+
+export default Account;
