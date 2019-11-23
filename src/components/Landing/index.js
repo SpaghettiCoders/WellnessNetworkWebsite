c5d414d677d645b7d75d6b77bc2b66c32ae85905
quade <qkirby@ufl.edu>
2019-11-22 19:37:04 -0500

    CONTENT: adding some html and css to look at changes

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -3,7 +3,40 @@ import { Link } from 'react-router-dom';
 import * as ROUTES from '../../constants/routes';
 const Landing = () => (
     <div>
-        LANDING
+    <div className="col-md-4">
+    	<div className="container">
+
+    		<div className="row">
+
+    		<div className="col-md-8">
+       			 <h1 className="my-4">Page Heading
+          <small>Secondary Text</small>
+        </h1>
+
+        
+        <div className="card mb-4">
+          <img className="card-img-top" src="http://placehold.it/750x300" alt="Card image cap"/>
+          <div className="card-body">
+            <h2 className="card-title">Post Title</h2>
+            <p className="card-text">Lorem ipsum dolor sit amet, consectetur
+ adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis 
+soluta, a laboriosam. Dicta expedita corporis animi vero voluptate 
+voluptatibus possimus, veniam magni quis!</p>
+            <a href="#" className="btn btn-primary">Read More →</a>
+          </div>
+          <div className="card-footer text-muted">
+            Posted on January 1, 2017 by
+            <a href="#">Start Bootstrap</a>
+          </div>
+        </div>
+
+
+
+</div>
+
+        </div>
+       		</div>
+       	</div>
     </div>
 );
 export default Landing;
