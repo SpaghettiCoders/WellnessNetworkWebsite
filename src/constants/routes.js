8aec5829c9572f5338393f25e3faeb5aea3e89ff
Alex Scardina <Alex@Alexs-MacBook-Pro.local>
2019-12-09 16:53:53 -0500

    minor fixes

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -5,8 +5,4 @@ export const HOME = '/home';
 export const ACCOUNT = '/account';
 export const ADMIN = '/admin';
 export const PASSWORD_FORGET = '/pw-forget';
-<<<<<<< HEAD
-export const CONTACT = '/contact';
-=======
 export const NEWSLETTER = '/newsletters'
->>>>>>> b177a6547b6bc5cec5775f8c2bed563bbd165480
